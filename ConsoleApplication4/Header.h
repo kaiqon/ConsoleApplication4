abda389cb7658317f8cbfd38dd07574a9c548345
Николай Жуков <zhukovnikolay66@gmail.com>
2024-03-06 13:23:16 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/Header.h
@@ -0,0 +1,7 @@
+#pragma once
+struct Student {
+	int id{}; 	char fio[50] = " ";  	int god{}; 	char pol{};
+	int ves{}; double rost{}; 	double stependia{}; 	char  gruppa[14] = "  ";
+	long long tel{}; string predmeti[3][6]; int marks[5];
+};
+
